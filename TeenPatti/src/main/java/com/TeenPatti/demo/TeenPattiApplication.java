7db5e6c87f4982ff47b84480d07b4d71c8505834
dravin <dravin@gmail.com>
2021-05-05 20:22:23 +0530

    Start project

---
+++ TeenPatti/src/main/java/com/TeenPatti/demo/TeenPattiApplication.java
@@ -0,0 +1,13 @@
+package com.TeenPatti.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenPattiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenPattiApplication.class, args);
+	}
+
+}
---
+++ TeenPatti/src/test/java/com/TeenPatti/demo/TeenPattiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeenPatti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenPattiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
